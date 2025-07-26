81d3c788226c61155c4eae4e5ca79250d908e5c6
Gian Luca <cecchi_gl@yahoo.it>
2025-07-26 10:53:50 +0200

    Update V2__init.sql

--- sql/V2__init.sql
+++ sql/V2__init.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `settings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `client` int DEFAULT NULL,
-  `field` varchar(12) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `field` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `value` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `client` (`client`),
   KEY `field` (`field`),
   CONSTRAINT `settings_ibfk_1` FOREIGN KEY (`client`) REFERENCES `clients` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
 INSERT INTO `settings` (`id`, `client`, `field`, `value`) VALUES (NULL, NULL, 'os.indextemplate', '{\"index_patterns\":[\"users-logs-*\"],\"template\":{\"settings\":{\"index.number_of_shards\":\"1\",\"index.number_of_replicas\":\"1\",\"index.plugins.index_state_management.policy_id\":\"user-logs-rollover-policy\"},\"mappings\":{\"dynamic\":true,\"properties\":{\"date\":{\"type\":\"date\"},\"googleTopics\":{\"type\":\"nested\",\"properties\":{\"name\":{\"type\":\"keyword\"},\"id\":{\"type\":\"integer\"}}},\"product\":{\"dynamic\":true,\"type\":\"nested\",\"properties\":{\"quantity\":{\"type\":\"integer\"},\"price\":{\"type\":\"object\",\"properties\":{\"amount\":{\"type\":\"float\"},\"currency\":{\"type\":\"keyword\"}}},\"id\":{\"type\":\"keyword\"},\"category\":{\"type\":\"keyword\"},\"brand\":{\"type\":\"keyword\"}}},\"instance\":{\"type\":\"integer\"},\"session\":{\"type\":\"keyword\"},\"target\":{\"type\":\"keyword\"},\"geo\":{\"dynamic\":true,\"type\":\"object\",\"properties\":{\"country\":{\"type\":\"keyword\"},\"zipCode\":{\"type\":\"keyword\"},\"city\":{\"type\":\"keyword\"},\"timeZone\":{\"type\":\"keyword\"},\"region\":{\"type\":\"keyword\"},\"point\":{\"type\":\"geo_point\"}}},\"referrer\":{\"type\":\"keyword\"},\"client\":{\"type\":\"integer\"},\"page\":{\"dynamic\":true,\"type\":\"object\",\"properties\":{\"image\":{\"type\":\"keyword\"},\"description\":{\"type\":\"text\"},\"href\":{\"type\":\"keyword\"},\"title\":{\"type\":\"text\"},\"type\":{\"type\":\"keyword\"}}},\"event\":{\"type\":\"keyword\"},\"device\":{\"dynamic\":true,\"type\":\"object\",\"properties\":{\"os\":{\"type\":\"keyword\"},\"browser\":{\"type\":\"keyword\"},\"ip\":{\"type\":\"ip\"},\"userAgent\":{\"fields\":{\"raw\":{\"type\":\"keyword\"}},\"type\":\"text\",\"store\": false},\"id\":{\"type\":\"keyword\"},\"type\":{\"type\":\"keyword\"}}},\"order\":{\"type\":\"keyword\"}}},\"aliases\":{\"users-logs\":{}}},\"composed_of\":[],\"priority\":\"0\",\"_meta\":{\"flow\":\"simple\"}}');
 INSERT INTO `settings` (`id`, `client`, `field`, `value`) VALUES (NULL, NULL, 'os.indexstatemanagement', '{\"policy\":{\"description\":\"rollover users-logs indexes\",\"default_state\":\"hot\",\"states\":[{\"name\":\"hot\",\"actions\":[{\"retry\":{\"count\":3,\"backoff\":\"exponential\",\"delay\":\"1m\"},\"rollover\":{\"min_size\":\"20gb\",\"min_index_age\":\"30d\",\"copy_alias\":true}}],\"transitions\":[{\"state_name\":\"delete\",\"conditions\":{\"min_index_age\":\"1096d\"}}]},{\"name\":\"delete\",\"actions\":[{\"retry\":{\"count\":3,\"backoff\":\"exponential\",\"delay\":\"1m\"},\"delete\":{}}],\"transitions\":[]}],\"ism_template\":[{\"index_patterns\":[\"users-logs-*\"],\"priority\":1}]}}');
@@ -32,4 +32,4 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`),
   CONSTRAINT `users_chk_1` CHECK ((`status` in (_utf8mb4'active',_utf8mb4'inactive',_utf8mb4'blocked')))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
