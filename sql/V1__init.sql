a921db8591408d6506ac3625fecbbf2d67effebd
Gian Luca <cecchi_gl@yahoo.it>
2025-07-26 15:59:29 +0200

    Update V1__init.sql

--- sql/V1__init.sql
+++ sql/V1__init.sql
@@ -5,8 +5,8 @@ USE cdpforge;
 CREATE TABLE `clients` (
   `id` BIGINT AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
@@ -15,8 +15,8 @@ CREATE TABLE `instances` (
   `client` INT NOT NULL,
   `description` VARCHAR(100) DEFAULT NULL,
   `active` TINYINT NOT NULL DEFAULT 1,  
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   INDEX `fk_instances_clients_idx` (`client`),
   CONSTRAINT `fk_instances_clients`
@@ -33,8 +33,8 @@ CREATE TABLE `plugins` (
   `priority` INT NOT NULL,
   `input_topic` VARCHAR(100) NOT NULL,
   `output_topic` VARCHAR(100) DEFAULT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT NOW(),
-  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT `plugins_pk` PRIMARY KEY (`id`),
   CONSTRAINT `plugins_unique` UNIQUE (`name`)
 );
