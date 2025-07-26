c1f5502d523e1d56c2339fddd31a7d13fcce718f
Gian Luca <cecchi_gl@yahoo.it>
2025-07-26 15:57:33 +0200

    Update V1__init.sql

--- sql/V1__init.sql
+++ sql/V1__init.sql
@@ -1,195 +1,40 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS cdpforge;
 
---
--- Database: `cdpforge`
---
-CREATE DATABASE IF NOT EXISTS `cdpforge` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `cdpforge`;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `clients`
---
+USE cdpforge;
 
 CREATE TABLE `clients` (
-  `id` int NOT NULL,
-  `name` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `flyway_schema_history`
---
-
-CREATE TABLE `flyway_schema_history` (
-  `installed_rank` int NOT NULL,
-  `version` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `description` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `script` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
-  `checksum` int DEFAULT NULL,
-  `installed_by` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `installed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `execution_time` int NOT NULL,
-  `success` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `instances`
---
+  `id` BIGINT AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE `instances` (
-  `id` int NOT NULL,
-  `client` int NOT NULL,
-  `description` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `active` tinyint NOT NULL DEFAULT '1',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `plugins`
---
+  `id` BIGINT AUTO_INCREMENT NOT NULL,
+  `client` INT NOT NULL,
+  `description` VARCHAR(100) DEFAULT NULL,
+  `active` TINYINT NOT NULL DEFAULT 1,  
+  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`id`),
+  INDEX `fk_instances_clients_idx` (`client`),
+  CONSTRAINT `fk_instances_clients`
+    FOREIGN KEY (`client`)
+    REFERENCES `clients` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
 
 CREATE TABLE `plugins` (
-  `id` bigint NOT NULL,
-  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `type` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `priority` int NOT NULL,
-  `input_topic` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `output_topic` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `settings`
---
-
-CREATE TABLE `settings` (
-  `id` int NOT NULL,
-  `client` int DEFAULT NULL,
-  `field` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `value` mediumtext COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint UNSIGNED NOT NULL,
-  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `is_super_admin` tinyint(1) DEFAULT '0',
-  `status` varchar(20) COLLATE utf8mb4_general_ci DEFAULT 'active',
-  `last_login_at` timestamp NULL DEFAULT NULL,
-  `reset_token` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `reset_token_expiry` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ;
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indici per le tabelle `flyway_schema_history`
---
-ALTER TABLE `flyway_schema_history`
-  ADD PRIMARY KEY (`installed_rank`),
-  ADD KEY `flyway_schema_history_s_idx` (`success`);
-
---
--- Indici per le tabelle `instances`
---
-ALTER TABLE `instances`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_instances_clients_idx` (`client`);
-
---
--- Indici per le tabelle `plugins`
---
-ALTER TABLE `plugins`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `plugins_unique` (`name`);
-
---
--- Indici per le tabelle `settings`
---
-ALTER TABLE `settings`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `client` (`client`),
-  ADD KEY `field` (`field`);
-
---
--- Indici per le tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `plugins`
---
-ALTER TABLE `plugins`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `settings`
---
-ALTER TABLE `settings`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `instances`
---
-ALTER TABLE `instances`
-  ADD CONSTRAINT `fk_instances_clients` FOREIGN KEY (`client`) REFERENCES `clients` (`id`);
-
---
--- Limiti per la tabella `settings`
---
-ALTER TABLE `settings`
-  ADD CONSTRAINT `settings_ibfk_1` FOREIGN KEY (`client`) REFERENCES `clients` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
+  `id` BIGINT AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  `priority` INT NOT NULL,
+  `input_topic` VARCHAR(100) NOT NULL,
+  `output_topic` VARCHAR(100) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT NOW(),
+  `updated_at` DATETIME NOT NULL DEFAULT NOW(),
+  CONSTRAINT `plugins_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `plugins_unique` UNIQUE (`name`)
+);
